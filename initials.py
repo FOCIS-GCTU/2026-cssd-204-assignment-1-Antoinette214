2f7a928dda309db20337f4c7d307f43a283711dc
Antoinette Baidoo <2425401488@live.gctu.edu.gh>
2026-05-31 00:33:31 -0700

    Assignment1

---
+++ initials.py
@@ -0,0 +1,36 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Antoinette Baidoo
+# STUDENT ID: 2425401488
+# Email: 2425401488@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Antoinette Baidoo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # main function prints initials
+
+
+    print()
+    print("...AB")
+    print()
+
+    print("...AAAAAAAAAAAA........BBBBBBBBBBBB.....")
+    print("...AAAA....AAAA........BBBB....BBBB.....")
+    print("...AAAA....AAAA........BBBB....BBBB.....")
+    print("...AAAAAAAAAAAA........BBBBBBBBBBBB.....")
+    print("...AAAA....AAAA........BBBB....BBBB.....")
+    print("...AAAA....AAAA........BBBB....BBBB.....")
+    print("...AAAA....AAAA........BBBB....BBBB.....")
+    print("...AAAA....AAAA........BBBB....BBBB.....")
+    print("...AAAA....AAAA...**...BBBB....BBBB...**")
+    print("...AAAA....AAAA...**...BBBBBBBBBBBB...**")
+
+    print()
+
+
+main()
