7fec60701f27f923c2f07d1678086833b2d6c45c
Antoinette214 <2425401488@live.gctu.edu.gh>
2026-06-02 16:12:02 +0000

    Deleted initial code

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
 
+# File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: Antoinette Baidoo
+# STUDENT ID:  2425401488
+# Email: 2425401488@live.edu.gctu.gh
+# Grader: Augustus
+#
+# On my honor, Antoinette Baidoo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+ 
+ 
 def main():
-  pass #code goes here
-
-
-
-main()
+    # Print the initials A.A.B. first in small form, then in large block letters.
+    # Each large letter is 12 characters wide by 10 characters high and is made up
+    # of the character matching that initial. Each letter is followed by a 2x2
+    # asterisk period block. Three period-columns separate each letter/period group.
+    # There are no trailing spaces on any line.
+ 
+    print()
+    print("...AAB")
+    print()
+ 
+    print(".......AAAA................AAAA............BBBBBBBBB........")
+    print("......AAAAAA..............AAAAAA...........BB.......BB......")
+    print(".....AA....AA............AA....AA..........BB.......BB......")
+    print("....AA......AA..........AA......AA.........BBBBBBBBB........")
+    print("...AAAAAAAAAAAA........AAAAAAAAAAAA........BBBBBBBBB   .....")
+    print("...AAAAAAAAAAAA........AAAAAAAAAAAA........BB........BB.....")
+    print("...AA........AA........AA........AA........BB........BB.....")
+    print("...AA........AA........AA........AA........BB........BB.....")
+    print("...AA........AA...**...AA........AA...**...BB........BB...**")
+    print("...AA........AA...**...AA........AA...**...BBBBBBBBB......**")
+ 
+    print()
+ 
